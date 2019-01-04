89e71bf6e3a7fb642d689c6a0078d23026bbc3d5
Sheldier <ashevale@mail.ru>
2019-01-04 15:28:01 +0300

    Created a changing button

--- js.js
+++ js.js
@@ -1,12 +1,18 @@
 const fill = document.querySelector('.fill');
 fill.style.backgroundImage = "url('img/vendor.png')";
 const empties = document.querySelectorAll('.empty');
+const button = document.querySelector('button');
 
 for(const empty of empties){
 	empty.addEventListener('dragover', dragOver);
 	empty.addEventListener('dragenter', dragEnter);
 	empty.addEventListener('dragleave', dragLeave);
 	empty.addEventListener('drop', dragDrop);
+	empty.addEventListener('dragstart', dragStartConts);
+}
+
+function dragStartConts() {
+	this.style.transform = null;
 }
 
 function dragStart() {
@@ -36,4 +42,28 @@ function dragDrop() {
 	this.className = 'empty';
 	this.append(fill);
 	this.style.transform = 'scale(0.65)';
+}
+function pressedBtn() {
+	if (fill.draggable == true){
+	fill.draggable = false;
+	fill.style.display = 'none';
+	button.dataset.switch = "on";
+	button.innerText = 'Changed to containers'
+	button.style.width = "200px";
+	empties.forEach(function(n) {
+		n.setAttribute('draggable','true');
+		n.style.transform = 'scale(1)';
+	});
+	}
+	else{
+		fill.draggable = true;
+		fill.style.display = 'block';
+		button.dataset.switch = "off";
+		button.innerText = 'Changed to picture'
+		button.style.width = "120px";
+		empties.forEach(function(n) {
+		n.setAttribute('draggable','false');
+
+	});
+	}
 }
